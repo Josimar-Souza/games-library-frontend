574522dfe9a5a2a3b2b0bff47b76504a45055dbc
Josimar Souza <contact.josimarsouza@gmail.com>
2022-10-10 15:18:02 -0300

    Added tests for page behaviour

--- src/tests/loginPageTests/loginPage.test.jsx
+++ src/tests/loginPageTests/loginPage.test.jsx
@@ -1,8 +1,12 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 import renderWithRouter from '../helpers/renderWithRouter';
 import pages from '../../pages';
+
+const emailTestId = 'email-input-login';
 
 describe('Testes da página de login', () => {
   describe('Verifica a existência dos elementos', () => {
@@ -17,7 +21,7 @@ describe('Testes da página de login', () => {
     });
 
     it('Verifica se existe um input para o email', async () => {
-      const emailInput = await screen.findByTestId('email-input-login');
+      const emailInput = await screen.findByTestId(emailTestId);
 
       expect(emailInput).toBeInTheDocument();
     });
@@ -38,6 +42,32 @@ describe('Testes da página de login', () => {
       const registerButton = await screen.findByRole('button', { name: 'Registrar' });
 
       expect(registerButton).toBeInTheDocument();
+    });
+  });
+
+  describe('Verifica o comportamento da página', () => {
+    beforeEach(() => {
+      renderWithRouter(<pages.LoginPage />);
+    });
+
+    it('Verifica se ao digitar um email inválido, o input fica vermelho', async () => {
+      const emailInput = await screen.findByTestId(emailTestId);
+
+      await act(async () => {
+        userEvent.type(emailInput, 'email.email');
+      });
+
+      expect(emailInput).toHaveStyle('border: 1px solid red');
+    });
+
+    it('Verifica se ao digitar um email válido, o input fica verde', async () => {
+      const emailInput = await screen.findByTestId(emailTestId);
+
+      await act(async () => {
+        userEvent(emailInput, 'testemail@gmail.com');
+      });
+
+      expect(emailInput).toHaveStyle('border: 1px solid green');
     });
   });
 });
