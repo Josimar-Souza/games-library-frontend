898fe8982d722aa080a6e735ad4a8855c737eba8
Josimar Souza <contact.josimarsouza@gmail.com>
2022-10-10 16:32:36 -0300

    Added tests for the buttons and tests refactored

--- src/tests/loginPageTests/loginPage.test.jsx
+++ src/tests/loginPageTests/loginPage.test.jsx
@@ -2,12 +2,12 @@
 import React from 'react';
 import { findByTestId, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { act } from 'react-dom/test-utils';
 import renderWithRouter from '../helpers/renderWithRouter';
 import pages from '../../pages';
 
 const emailInputTestId = 'email-input-login';
 const passwordInputTestId = 'password-input-login';
+const feedbackTestId = 'feedback-login';
 
 describe('Testes da página de login', () => {
   describe('Verifica a existência dos elementos', () => {
@@ -39,8 +39,8 @@ describe('Testes da página de login', () => {
       expect(signInButton).toBeInTheDocument();
     });
 
-    it('Verifica se existe um botão escrito "Registrar"', async () => {
-      const registerButton = await screen.findByRole('button', { name: 'Registrar' });
+    it('Verifica se existe um botão escrito "Registre-se"', async () => {
+      const registerButton = await screen.findByRole('button', { name: 'Registre-se' });
 
       expect(registerButton).toBeInTheDocument();
     });
@@ -54,9 +54,7 @@ describe('Testes da página de login', () => {
     it('Verifica se ao digitar um email inválido, o input fica vermelho', async () => {
       const emailInput = await screen.findByTestId(emailInputTestId);
 
-      await act(async () => {
-        userEvent.type(emailInput, 'email.email');
-      });
+      userEvent.type(emailInput, 'email.email');
 
       expect(emailInput).toHaveStyle(`
         border: 1px solid red;
@@ -67,9 +65,7 @@ describe('Testes da página de login', () => {
     it('Verifica se ao digitar um email válido, o input fica verde', async () => {
       const emailInput = await screen.findByTestId(emailInputTestId);
 
-      await act(async () => {
-        userEvent(emailInput, 'testemail@gmail.com');
-      });
+      userEvent(emailInput, 'testemail@gmail.com');
 
       expect(emailInput).toHaveStyle(`
         border: 1px solid green;
@@ -80,9 +76,7 @@ describe('Testes da página de login', () => {
     it('Verifica se ao digitar uma senha inválida, o input fica vermelho', async () => {
       const passwordInput = await findByTestId(passwordInputTestId);
 
-      await act(async () => {
-        userEvent.type(passwordInput, 'Kfnoiadiodw');
-      });
+      userEvent.type(passwordInput, 'Kfnoiadiodw');
 
       expect(passwordInput).toHaveStyle(`
         border: 1px solid red;
@@ -93,14 +87,50 @@ describe('Testes da página de login', () => {
     it('Verifica se ao digitar um senha válida, o input fica verde', async () => {
       const passwordInput = await findByTestId(passwordInputTestId);
 
-      await act(async () => {
-        userEvent.type(passwordInput, 'TestePass15!');
-      });
+      userEvent.type(passwordInput, 'TestePass15!');
 
       expect(passwordInput).toHaveStyle(`
         border: 1px solid green;
         box-shadow: none;
       `);
+    });
+
+    it('Verifica se ao clicar no botão "Logar" com valores inválidos, uma mensagem aparece', async () => {
+      const signInButton = await screen.findByRole('button', { name: 'Logar' });
+      const emailInput = await screen.findByTestId(emailInputTestId);
+      const passwordInput = await findByTestId(passwordInputTestId);
+
+      userEvent.type(emailInput, 'testemail.gmail.com');
+      userEvent.type(passwordInput, 'TestePass1554875!');
+      userEvent.click(signInButton);
+
+      const feedbackMessage = await screen.findByTestId(feedbackTestId);
+
+      expect(feedbackMessage.textContent).toBe('Email ou senha inválidos');
+    });
+
+    it('Verifica se ao clicar no botão "Logar" com valores válidos, a página é redirecionada', async () => {
+      const signInButton = await screen.findByRole('button', { name: 'Logar' });
+      const emailInput = await screen.findByTestId(emailInputTestId);
+      const passwordInput = await findByTestId(passwordInputTestId);
+
+      userEvent.type(emailInput, 'testemail@gmail.com');
+      userEvent.type(passwordInput, 'TestePass15!');
+      userEvent.click(signInButton);
+
+      const { pathname } = window.location;
+
+      expect(pathname).toBe('/games');
+    });
+
+    it('Verifica se ao clicar no botão "Registre-se", a página é redirecionada', async () => {
+      const registerButton = await screen.findByRole('button', { name: 'Registre-se' });
+
+      userEvent.click(registerButton);
+
+      const { pathname } = window.location;
+
+      expect(pathname).toBe('/register');
     });
   });
 });
