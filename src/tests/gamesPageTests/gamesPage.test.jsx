f28cfcfe9ac7283f37545706d28f1c027bcab0db
Josimar Souza <contact.josimarsouza@gmail.com>
2022-11-14 15:15:38 -0300

    Created tests for page header

---
+++ src/tests/gamesPageTests/gamesPage.test.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import pages from '../../pages';
+
+describe('Testes da página principal de games', () => {
+  describe('Verifica a existência dos elementos', () => {
+    beforeEach(() => {
+      renderWithRouter(<pages.GamesPage />);
+    });
+
+    it('A logo do site', async () => {
+      const pageLogo = await screen.findByAltText('Logo da página no header');
+
+      expect(pageLogo).toBeInTheDocument();
+    });
+
+    it('Um título escrito "Games Library"', async () => {
+      const headerTitle = await screen.findByRole('heading', { name: 'Games Library' });
+
+      expect(headerTitle).toBeInTheDocument();
+    });
+
+    it('Um botão para deslogar da aplicação escrito "Deslogar"', async () => {
+      const signOutButton = await screen.findByRole('button', { name: 'Deslogar' });
+
+      expect(signOutButton).toBeInTheDocument();
+    });
+  });
+});
