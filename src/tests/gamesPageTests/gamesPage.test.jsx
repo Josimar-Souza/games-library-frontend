eb9a7719379b53fd08b02c6a20359cafab172ab6
Josimar Souza <contact.josimarsouza@gmail.com>
2022-11-15 14:58:06 -0300

    Created tests for categories and game cards

--- src/tests/gamesPageTests/gamesPage.test.jsx
+++ src/tests/gamesPageTests/gamesPage.test.jsx
@@ -3,6 +3,14 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouter from '../helpers/renderWithRouter';
 import pages from '../../pages';
+import mockCategories from '../mocks/mockCategories';
+import mockGames from '../mocks/mockGames';
+
+const testIds = {
+  gameCardImage: 'game-card-image',
+  gameCardTitle: 'game-card-title',
+  gameCardDetailsButton: 'game-card-detail-button',
+};
 
 describe('Testes da página principal de games', () => {
   describe('Verifica a existência dos elementos', () => {
@@ -38,6 +46,49 @@ describe('Testes da página principal de games', () => {
       const searchButton = await screen.findByRole('button', { name: 'Pesquisar' });
 
       expect(searchButton).toBeInTheDocument();
+    });
+
+    it('Todas as categorias com as informações corretas', async () => {
+      const categoriesPromises = [];
+
+      mockCategories.forEach(({ category }) => {
+        const categoryElement = screen.findByText(category);
+
+        categoriesPromises.push(categoryElement);
+      });
+
+      const categories = await Promise.all(categoriesPromises);
+
+      categories.forEach((category, index) => {
+        expect(category.textContent).toBe(mockCategories[index].category);
+      });
+    });
+
+    it('Todos os cards de games com as informações corretas', async () => {
+      const { games } = mockGames;
+      const gamesImagesPromises = [];
+      const gamesTitlesPromises = [];
+      const gamesDetailsButtonsPromises = [];
+
+      games.forEach((_title, index) => {
+        const gameImageElement = screen.findByTestId(`${testIds.gameCardImage}-${index}`);
+        const gameTitleElement = screen.findByTestId(`${testIds.gameCardTitle}-${index}`);
+        const gameDetailsButtonElement = screen.findByTestId(`${testIds.gameCardDetailsButton}-${index}`);
+
+        gamesImagesPromises.push(gameImageElement);
+        gamesTitlesPromises.push(gameTitleElement);
+        gamesDetailsButtonsPromises.push(gameDetailsButtonElement);
+      });
+
+      const gamesImages = await Promise.all(gamesImagesPromises);
+      const gamesTitles = await Promise.all(gamesTitlesPromises);
+      const gamesDetailsButtons = await Promise.all(gamesDetailsButtonsPromises);
+
+      for (let index = 0; index < games.length; index += 1) {
+        expect(gamesImages[index]).toHaveAttribute('src', games[index].image);
+        expect(gamesTitles[index].textContent).toBe(games[index].title);
+        expect(gamesDetailsButtons[index]).toBeInTheDocument();
+      }
     });
   });
 });
