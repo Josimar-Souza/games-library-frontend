9b0f876d4aa501c1eb688421802e2b09ae701895
Josimar Souza <contact.josimarsouza@gmail.com>
2022-11-16 13:52:39 -0300

    Added background image to games page

--- src/pages/GamesPage/gamesPageStyles.js
+++ src/pages/GamesPage/gamesPageStyles.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
+import background from '../../images/Background.jpg';
 
-const GamesPageSection = styled.section``;
+const GamesPageSection = styled.section`
+  background-image: url(${background});
+  background-size: cover;
+  max-width: 100%;
+  min-height: 100vh;
+  width: 100%;
+`;
 
 export default GamesPageSection;
