806fa1dfa2b3ce56c5dc79a173e8e3274e1a1828
Josimar Souza <contact.josimarsouza@gmail.com>
2022-11-18 14:52:01 -0300

    Created method to log out

--- src/pages/GamesPage/index.jsx
+++ src/pages/GamesPage/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import GamesPageSection from './gamesPageStyles';
 import components from '../../components';
+import { removeItem } from '../../helpers/localStorageManager';
 
 function GamesPage() {
+  const navigate = useNavigate();
+
   const { Header } = components;
+
+  const onLogOutButtonClick = () => {
+    removeItem('token');
+
+    navigate('/');
+  };
 
   return (
     <GamesPageSection>
@@ -11,6 +21,7 @@ function GamesPage() {
         headerFontColor="white"
         headerTitle="Games Library"
         headerButtonText="Deslogar"
+        onHeaderButtonClick={onLogOutButtonClick}
       />
     </GamesPageSection>
   );
