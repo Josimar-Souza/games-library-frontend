d88d2d33358953f94a808f7079bf03abbc01aa6e
Josimar Souza <contact.josimarsouza@gmail.com>
2022-12-01 16:16:33 -0300

    Added useEffect to get game details

--- src/pages/DetailsPage/index.jsx
+++ src/pages/DetailsPage/index.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import DetailsPageSection from './detailsPageStyles';
+import GamesAPI from '../../api/gamesAPI';
+import { getItem } from '../../helpers/localStorageManager';
+
+const baseUrl = process.env.REACT_APP_API_URL;
+const gamesAPI = new GamesAPI(baseUrl, 15000);
 
 function DetailsPage() {
+  const [gameDetails, setGameDetails] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getGameDetails = async () => {
+      const details = await gamesAPI.getGameById(id, getItem('token'));
+
+      setGameDetails(details);
+    };
+
+    getGameDetails();
+  }, []);
+
   return (
     <DetailsPageSection />
   );
