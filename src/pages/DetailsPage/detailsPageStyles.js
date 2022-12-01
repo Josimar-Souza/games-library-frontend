3a569f0c1c9914e1a3e62701311aba4c673383c6
Josimar Souza <contact.josimarsouza@gmail.com>
2022-12-01 17:06:56 -0300

    Added title and sinopse

--- src/pages/DetailsPage/detailsPageStyles.js
+++ src/pages/DetailsPage/detailsPageStyles.js
@@ -1,11 +1,24 @@
 import styled from 'styled-components';
 
-const DetailsPageSection = styled.section`
+export const DetailsPageSection = styled.section`
+  align-items: center;
+  display: flex;
   background-image: url(${({ backgroundImage }) => backgroundImage});
   background-size: cover;
+  justify-content: center;
   max-width: 100%;
   min-height: 100vh;
   width: 100%;
 `;
 
-export default DetailsPageSection;
+export const InfoPainel = styled.div`
+  align-items: center;
+  display: flex;
+  background-color: rgba(255, 255, 255, 0.7);
+  border-radius: 15px;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 100%;
+  padding: 10px;
+  width: 80%;
+`;
--- src/pages/DetailsPage/index.jsx
+++ src/pages/DetailsPage/index.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import DetailsPageSection from './detailsPageStyles';
 import GamesAPI from '../../api/gamesAPI';
 import { getItem } from '../../helpers/localStorageManager';
+import component from '../../components';
+import {
+  DetailsPageSection,
+  InfoPainel,
+} from './detailsPageStyles';
 
 const baseUrl = process.env.REACT_APP_API_URL;
 export const gamesAPI = new GamesAPI(baseUrl, 15000);
@@ -10,6 +14,11 @@ export const gamesAPI = new GamesAPI(baseUrl, 15000);
 function DetailsPage() {
   const [gameDetails, setGameDetails] = useState({});
   const { id } = useParams();
+
+  const {
+    Title,
+    Paragraph,
+  } = component;
 
   useEffect(() => {
     const getGameDetails = async () => {
@@ -25,7 +34,24 @@ function DetailsPage() {
     <DetailsPageSection
       backgroundImage={gameDetails.backdrop}
       data-testid="game-details-backdrop"
-    />
+    >
+      <InfoPainel>
+        <Title
+          textAlign="center"
+          width="100%"
+          margin="0.5rem 0"
+        >
+          {gameDetails.title}
+        </Title>
+        <Paragraph
+          width="100%"
+          textAlign="center"
+          margin="0.5rem 0"
+        >
+          {gameDetails.sinopse}
+        </Paragraph>
+      </InfoPainel>
+    </DetailsPageSection>
   );
 }
 
